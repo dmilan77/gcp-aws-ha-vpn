8fff6dc343a2c591e02c9cffd6e549884f260f7d
dmilan77 <milan.das77@gmail.com>
2022-11-19 23:20:02 -0500

    added ..

--- 3-gcp/vpn-tunnels.tf
+++ 3-gcp/vpn-tunnels.tf
@@ -5,7 +5,7 @@ resource "google_compute_vpn_tunnel" "tunnel1" {
   region                          = var.gcp_region
   vpn_gateway                     = var.gcp_vpn_gateway_id
   peer_external_gateway           = google_compute_external_vpn_gateway.external_gateway.id
-  peer_external_gateway_interface = count.index
+  peer_external_gateway_interface = 0
   shared_secret                   = var.preshared_key
   router                          = var.gcp_cloud_router_id
   vpn_gateway_interface           = count.index
